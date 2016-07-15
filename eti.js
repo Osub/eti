56da8075c2b514ceeacfb69c64735fafee1ad92f
osub <814566123@qq.com>
2016-07-15 17:01:22 +0800

    英文与数字的转换(26进制与10进制的转换)

--- eti.js
+++ eti.js
@@ -2,25 +2,42 @@ module.exports = function(debug,is_low){
     var pos = 1,        //位数
         remainder, //余数
         quotient;  //商
-        return strPosChar = function(index,str){
-            if(is_low==='D'){
+    if(debug==='de') {
+        return strPosChar = function (index, str) {
+            if (is_low === 'D') {
                 start = 64;
-            }else{
+            } else {
                 start = 96;
             }
-            remainder = index%26;
-            quotient  = Math.floor(index/26);
-            if(quotient>26){
-                str += String.fromCharCode(remainder+start);
+            remainder = index % 26;
+            quotient = Math.floor(index / 26);
+            if (quotient > 26) {
+                str += String.fromCharCode(remainder + start);
                 pos++;
+                strPosChar(quotient, str);
+            } else {
+                str += String.fromCharCode(remainder + start);
+
+                if (quotient) str += String.fromCharCode(quotient + start);
+
+                str = str.split("").reverse().join("");
                 console.log(str);
-                strPosChar(quotient,str);
-            }else{
-                str += String.fromCharCode(remainder+start);
-                console.log(str);
-                if(quotient) str += String.fromCharCode(quotient+start);
-                console.log(str);
-                return str.split(" ").reverse().join(" ");
+                return str;
             }
         };
+    }else if(debug==='en'){
+       return function (stri) {
+           var map_arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+               char_pos = function (current) {
+                   return map_arr.lastIndexOf(current) + 1;
+               }
+           var str_len = stri.length,
+               res = 0;
+           for(var i=1;i<=str_len;i++){
+              res += Math.pow(26,str_len-i)*char_pos(stri[i-1].toUpperCase());
+           }
+           console.log(res);
+           return   res;
+       }
+    }
 };
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var eti = require('./eti');
-eti('debug','D')(18801158349,'');
-eti('debug','X')(18801158349,'');
+eti('de','D')(9,'');
+eti('de','D')(221629,'');
+eti('de','D')(17311,'');
+eti('en','X')('i');
+eti('en','X')('love');
+eti('en','X')('you');
 
 
 
